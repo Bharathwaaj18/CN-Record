42f4f23ed7c43be320bb21d78efbff18b5497dd8
Bharathwaaj18 <122618653+Bharathwaaj18@users.noreply.github.com>
2024-11-10 22:19:48 +0530

    Delete

--- CN_RECORD/Exp8 RS232C/8A/Rs232c.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-
-public class Rs232c{
-    public static void main(String[] args) {
-        System.out.println("Transfered");
-
-    }
-}
--- CN_RECORD/Exp9 RMI/myinterface.java
+++
@@ -1,5 +0,0 @@
- import java.rmi.*;
- public interface myinterface extends Remote
- {
- public String countInput(String input)throws RemoteException;
- }
